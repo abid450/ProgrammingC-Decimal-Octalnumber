ffec438c2de7dbbd19502db8e7d090be5a272645
Abid Hasan <144882936+abid450@users.noreply.github.com>
2023-09-29 11:37:14 +0600

    I have a upload Programming C file

---
+++ main.c
@@ -0,0 +1,33 @@
+
+
+  /* Octal to Decimal  number */
+
+ 
+      #include<stdio.h>
+
+      int main(){
+        int number;
+
+        printf("octal number = ");
+        scanf("%o",&number);
+        printf("Decimal number = %d",number);
+        getchar();
+      }
+
+      /* Decimal to octal number */
+
+
+      #include<stdio.h>
+       
+       int main(){
+        int number;
+
+        printf("Desimal number = ");
+        scanf("%d",&number);
+        printf("Octal number =%o",number);
+        getchar();
+
+       }
+
+ 
+  
